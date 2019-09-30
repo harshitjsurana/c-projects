198b83ebd2b1a1850ae38b8d67b84447e320232c
harshitjsurana <55993308+harshitjsurana@users.noreply.github.com>
2019-09-30 22:37:17 +0530

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include<conio.h>
+#include<stdlib.h>
+using namespace std;
+
+int main()
+{
+    char ch;
+    cout << "Enter a character:";
+    cin>>ch;
+    if(ch>=65&&ch<=90)
+        cout<<"\n"<<"You entered a Uppercase character.";
+    else  if(ch>=97&&ch<=122)
+        cout<<"\n"<<"You entered a Lowercase character.";
+        else  if(ch>=48&&ch<=57)
+        cout<<"\n"<<"You entered a Digit.";
+        else cout<<"You entered a special character.";
+    getch();
+}
+
+//Process terminated with status -1073741510 (0 minute(s), 4 second(s))
+
